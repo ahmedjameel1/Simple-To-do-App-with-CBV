557b2f693d25fbbceb9055fbd33483d2daa8d5ea
gunnerisforher <roarme247@gmail.com>
2023-01-18 11:12:31 +0200

    update

--- api/urls.py
+++ api/urls.py
@@ -7,8 +7,8 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('routes/', views.GetRoutes.as_view(), name='routes'),
-    path('tasks/', views.TaskList.as_view(), name='tasks'),
-    path('task/<int:pk>/', views.TaskDetails.as_view(), name='task'),
+    path('tasks/', views.TaskList.as_view(), name='api-tasks'),
+    path('task/<int:pk>/', views.TaskDetails.as_view(), name='api-task'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
